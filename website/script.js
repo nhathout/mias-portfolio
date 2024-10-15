38b8c1b31a434da0d2675d6d82a72b536d7c45b4
nhathout <nhathout11@gmail.com>
2024-10-14 20:22:21 -0400

    ENHANCEMENT click to flip and mroe

--- website/script.js
+++ website/script.js
@@ -1,1 +1,11 @@
-//nothing yet
+// Get all artwork elements
+const artworks = document.querySelectorAll('.artwork');
+
+artworks.forEach((artwork) => {
+  artwork.addEventListener('click', () => {
+    const inner = artwork.querySelector(
+      '.artwork-inner, .artwork-inner1, .artwork-inner2, .artwork-inner3, .artwork-inner4, .artwork-inner5, .artwork-inner6, .artwork-inner7, .artwork-inner8'
+    );
+    inner.classList.toggle('flipped');
+  });
+});
